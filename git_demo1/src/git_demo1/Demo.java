298ee32c2c9cc83307e5ffb29e1679ba132bfd2b
anjaneyulu <anjaneyulu.savilla@htcindia.com>
2017-10-23 23:40:56 +0530

    output

--- git_demo1/src/git_demo1/Demo.java
+++ git_demo1/src/git_demo1/Demo.java
@@ -2,8 +2,8 @@ package git_demo1;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Testing....");
-		System.out.println("Testing....");
+		System.out.println("1 Testing....");
+		System.out.println("2 Testing....");
 		System.out.println("Testing....");
 		System.out.println("Testing....");
 	}
